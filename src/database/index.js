dee56d174dc2010fb0803be64e228dc7af4280ea
aawezk456 <aawezk456@gmail.com>
2021-10-25 22:36:58 +0530

    Update:Setup Completed Firebase with model

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,7 @@
 const firebase = require('firebase-admin');
 const config = require('../config');
-var serviceAccount = require("../../addons/service.json");
 
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://retailer-fd8bc-default-rtdb.firebaseio.com"
-});
-// firebase.initializeApp(config.firebaseConfig);
+firebase.initializeApp(config.firebaseConfig);
 
 const db = firebase.firestore();
 module.exports = db;
